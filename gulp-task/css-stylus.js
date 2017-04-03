5aedcb7c556eb03bc989de8d0127dd9bd6ef6c0b
sondmitryv <sondmitryv@gmail.com>
2017-04-03 22:29:11 +0300

    retina js img

--- gulp-task/css-stylus.js
+++ gulp-task/css-stylus.js
@@ -3,7 +3,7 @@ module.exports = function (gulp, plugins, helpers) {
         return gulp.src(plugins.config.get('stylus.buildPath'))
             .pipe(plugins.plumber({errorHandler: helpers.errorHandler}))
             .pipe(plugins.stylus(
-                {use: [plugins.poststylus(['lost', 'autoprefixer']), plugins.rupture()], import: ['rupture']}
+                {use: [plugins.poststylus(['lost', 'autoprefixer'])]}
             ))
             .pipe(gulp.dest(plugins.config.get('stylus.buildCompilePath')))
             .pipe(plugins.browserSync.stream());
